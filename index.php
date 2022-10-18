dc373f5ce25d1d5985fd1d7f1acfcbdf4d141624
Franck <depoorter.franck@gmail.com>
2022-10-18 11:23:14 +0200

    Create absract and final

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Bicycle.php';
+
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+echo '<br> Vitesse du vélo : ' . $bicycle->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bicycle->brake();
+echo '<br> Vitesse du vélo : ' . $bicycle->getcurrentSpeed() . ' km/h' . '<br>';
+echo $bicycle->brake() . '<br>';
+
+require_once 'Cars.php';
+
+$car = new Cars('green', 4, 'electric');
+echo $car->forward();
+echo '<br> Vitesse de la voiture : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake();
+echo '<br> Vitesse de la voiture : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake(). '<br>';
+
+require_once 'Trucks.php';
+$tanker = new Trucks('white', 3, 'fuel', 34000);
+echo  $tanker->forward();
+echo '<br> Vitesse du camion : ' . $tanker->getCurrentSpeed() . ' km/h' . '<br>';
+echo $tanker->brake();
+echo '<br> Vitesse du camion : ' . $tanker->getCurrentSpeed() . ' km/h' . '<br>';
+echo $tanker->brake();
+echo '<br> Chargement du camion: '. $tanker->getLoading(). ' litres' . '<br>';
+echo $tanker->loading(). '<br>';
+
